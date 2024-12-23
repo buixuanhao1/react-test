38def2718805f35fe5ad5746e52b37cfa91863c0
bui xuan hao <hbui40537@gmail.com>
2024-12-23 17:19:06 +0700

    1

---
+++ src/components/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>Hello my friend</div>
+        );
+    }
+}
+
+export default MyComponent
